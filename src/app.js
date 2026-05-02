4c0fe00409832ecae4d99521ddffae2eb79c6adf
Matt-Chee123 <cheematthew10@gmail.com>
2026-05-02 14:45:45 +0100

    Mount empty routes for funds, investments, and investors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const { pool } = require('./db');
 const { errorMiddleware } = require('./errors');
+const fundsRouter = require('./routes/funds.js');
+const investorsRouter = require('./routes/investors.js');
+const investmentsRouter = require('./routes/investments.js');
 
 const app = express();
 app.use(express.json());
@@ -13,6 +16,12 @@ app.get('/health', async (req, res, next) => {
         next(err);
     }
 });
+
+// app.use('/funds', fundsRouter)
+//
+// app.use('/investors', investorsRouter)
+//
+// app.use('/funds/:fund_id/investments', investmentsRouter)
 
 app.use(errorMiddleware);
 
